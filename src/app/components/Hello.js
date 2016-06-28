0c9560af9c3136fee142bf783cd6df92d74f2b28
Marcelo Vargas <marcelo.vargas90@gmail.com>
2016-06-28 18:35:17 -0300

    updated matches

--- src/app/components/Hello.js
+++ src/app/components/Hello.js
@@ -3,11 +3,11 @@ var TeamTable = require('./TeamTable');
 
 var teams = [
   {id: 1, name: 'Grêmio', position: 1, matchs: 6, points: 18},
-  {id: 2, name: 'Internacional', position: 2, matchs: 3, points: 15},
-  {id: 3, name: 'Flamengo', position: 3, matchs: 3, points: 12},
-  {id: 4, name: 'Fluminense', position: 4, matchs: 3, points: 9},
-  {id: 5, name: 'São Paulo', position: 5, matchs: 3, points: 6},
-  {id: 6, name: 'Santos', position: 6, matchs: 3, points: 3}
+  {id: 2, name: 'Internacional', position: 2, matchs: 6, points: 15},
+  {id: 3, name: 'Flamengo', position: 3, matchs: 6, points: 12},
+  {id: 4, name: 'Fluminense', position: 4, matchs: 6, points: 9},
+  {id: 5, name: 'São Paulo', position: 5, matchs: 6, points: 6},
+  {id: 6, name: 'Santos', position: 6, matchs: 6, points: 3}
 ];
 
 module.exports = React.createClass({
